753321b2e8eaa8bfaac74cf4e35f6b6cf69f5432
Muhammad Hewedy <mhewedy@gmail.com>
2020-04-26 06:37:17 +0300

    use 2nd level command with completion

--- cli/completion.go
+++ cli/completion.go
@@ -9,29 +9,38 @@ import (
 var completionCmd = &cobra.Command{
 	Use:   "completion",
 	Short: "Generates completion scripts (bash and zsh)",
-	Long: `To load completion run
-. <(vermin completion bash)
-or 
-. <(vermin completion zsh)
-
-You might consider adding it to your shell config file (.bashrc or .zshrc)
-`,
-	Run: func(cmd *cobra.Command, args []string) {
-		switch args[0] {
-		case "bash":
-			_ = rootCmd.GenBashCompletion(os.Stdout)
-		case "zsh":
-			_ = rootCmd.GenZshCompletion(os.Stdout)
-		}
+	Long:  `Generates completion scripts (bash and zsh)`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return errors.New("bash or zsh expected")
 	},
-	Args: func(cmd *cobra.Command, args []string) error {
-		if len(args) != 1 || !(args[0] == "bash" || args[0] == "zsh") {
-			return errors.New("should choose between bash or zsh")
-		}
-		return nil
+}
+
+var bashCmd = &cobra.Command{
+	Use:   "bash",
+	Short: "Generates bash completion scripts",
+	Long: `To load completion run:
+. <(vermin completion bash)
+
+You might consider adding it to your shell config file .bashrc
+`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return rootCmd.GenBashCompletion(os.Stdout)
+	},
+}
+
+var zshCmd = &cobra.Command{
+	Use:   "zsh",
+	Short: "Generates zsh completion scripts",
+	Long: `To load completion run:
+vermin completion zsh > ~/.zsh/completion/_vermin
+`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return rootCmd.GenZshCompletion(os.Stdout)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(completionCmd)
+	completionCmd.AddCommand(bashCmd)
+	completionCmd.AddCommand(zshCmd)
 }
