7a71de34d40c563799c8c8b3d0bc0e673f214192
Muhammad Hewedy <mhewedy@gmail.com>
2020-04-29 02:40:58 +0300

    fix download log issue

--- images/wget.go
+++ images/wget.go
@@ -5,5 +5,5 @@ package images
 import "github.com/mhewedy/vermin/cmd"
 
 func wget(url string, file string) (string, error) {
-	return cmd.Execute("wget", "-O", file, url)
+	return cmd.ExecuteP("wget", "-O", file, url)
 }
