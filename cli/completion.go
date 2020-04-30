841c5c4b0e6e00544c27b284d9d216a31bde06d3
Muhammad Hewedy <mhewedy@gmail.com>
2020-05-01 00:34:52 +0300

    enable PowerShell completion

--- cli/completion.go
+++ cli/completion.go
@@ -8,10 +8,10 @@ import (
 
 var completionCmd = &cobra.Command{
 	Use:   "completion",
-	Short: "Generates completion scripts (bash and zsh)",
-	Long:  `Generates completion scripts (bash and zsh)`,
+	Short: "Generates completion scripts (Bash, Zsh and PowerShell)",
+	Long:  `Generates completion scripts (Bash, Zsh and PowerShell)`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return errors.New("bash or zsh expected")
+		return errors.New("bash, zsh or powershell expected")
 	},
 }
 
@@ -39,8 +39,18 @@ vermin completion zsh > ~/.zsh/completion/_vermin
 	},
 }
 
+var powerShellCmd = &cobra.Command{
+	Use:   "powershell",
+	Short: "Generates PowerShell completion scripts",
+	Long:  `Generates PowerShell completion scripts`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return rootCmd.GenPowerShellCompletion(os.Stdout)
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(completionCmd)
 	completionCmd.AddCommand(bashCmd)
 	completionCmd.AddCommand(zshCmd)
+	completionCmd.AddCommand(powerShellCmd)
 }
