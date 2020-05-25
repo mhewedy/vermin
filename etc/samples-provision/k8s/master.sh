2b96fc9c6af8a4f489cd641aff2bbaad066889e3
Muhammad Hewedy <mhewedy@gmail.com>
2020-05-26 01:10:13 +0300

    fix k8s setup script

--- etc/samples-provision/k8s/master.sh
+++ etc/samples-provision/k8s/master.sh
@@ -1,6 +1,10 @@
 # HW requirements: 2 CPUS 2 RAM
 # OS: Ubuntu focal
 
+#should change to $(lsb_release -cs) but it appears it is not working on focal yet
+docker_ubuntu_release=bionic
+k8s_version=1.18.0
+
 # Set hostname and disable swap
 sudo hostnamectl set-hostname "$(hostname -I | awk '{print $1}')"
 sudo swapoff -a && sudo sed -i '/ swap / s/^/#/' /etc/fstab
@@ -20,16 +24,21 @@ sudo netplan apply
 ############# start k8s installation
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $docker_ubuntu_release \
+  stable"
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
-sudo apt-get update -y
-sudo apt-get install -y docker-ce=18.06.1~ce~3-0~ubuntu kubelet=1.13.5-00 kubeadm=1.13.5-00 kubectl=1.13.5-00
+sudo apt-get update
+sudo apt-get install -y \
+  containerd.io=1.2.13-1 \
+  docker-ce=5:19.03.8~3-0~ubuntu-$docker_ubuntu_release \
+  docker-ce-cli=5:19.03.8~3-0~ubuntu-$docker_ubuntu_release
+sudo apt-get install -y kubelet=$k8s_version-00 kubeadm=$k8s_version-00 kubectl=$k8s_version-00
 sudo apt-mark hold docker-ce kubelet kubeadm kubectl
 
 echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
@@ -37,10 +46,8 @@ sudo sysctl -p
 
 ####### configure kubeadm
 
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16 &> ~/kubeadm.log
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16 &>~/kubeadm.log
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown "$(id -u):$(id -g)" $HOME/.kube/config
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
-
-
--- etc/samples-provision/k8s/worker.sh
+++ etc/samples-provision/k8s/worker.sh
@@ -1,5 +1,9 @@
-# HW requirements: 2 CPUS 2 RAM
+# HW requirements: 1 CPUS 2 RAM
 # OS: Ubuntu focal
+
+#should change to $(lsb_release -cs) but it appears it is not working on focal yet
+docker_ubuntu_release=bionic
+k8s_version=1.18.0
 
 # Set hostname and disable swap
 sudo hostnamectl set-hostname "$(hostname -I | awk '{print $1}')"
@@ -20,16 +24,21 @@ sudo netplan apply
 ############# start k8s installation
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $docker_ubuntu_release \
+  stable"
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
-sudo apt-get update -y
-sudo apt-get install -y docker-ce=18.06.1~ce~3-0~ubuntu kubelet=1.13.5-00 kubeadm=1.13.5-00 kubectl=1.13.5-00
+sudo apt-get update
+sudo apt-get install -y \
+  containerd.io=1.2.13-1 \
+  docker-ce=5:19.03.8~3-0~ubuntu-$docker_ubuntu_release \
+  docker-ce-cli=5:19.03.8~3-0~ubuntu-$docker_ubuntu_release
+sudo apt-get install -y kubelet=$k8s_version-00 kubeadm=$k8s_version-00 kubectl=$k8s_version-00
 sudo apt-mark hold docker-ce kubelet kubeadm kubectl
 
 echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
