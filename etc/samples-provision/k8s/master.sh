636dce0adb64de6b11645f54f80b731bd0eb51f8
Muhammad Hewedy <mhewedy@gmail.com>
2020-05-26 01:26:46 +0300

    polish

--- etc/samples-provision/k8s/master.sh
+++ etc/samples-provision/k8s/master.sh
@@ -46,8 +46,11 @@ sudo sysctl -p
 
 ####### configure kubeadm
 
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16 &>~/kubeadm.log
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16 | tee ~/kubeadm.log
+
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown "$(id -u):$(id -g)" $HOME/.kube/config
+
+## install flannel network
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
