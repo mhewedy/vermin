6ba3f6dbeaab693ed7e3dc50a3572dfd5e0a7dd4
Muhammad Hewedy <mhewedy@gmail.com>
2020-08-29 15:42:57 +0300

    update gist

--- config/trace/trace.go
+++ config/trace/trace.go
@@ -2,6 +2,7 @@ package trace
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -11,8 +12,6 @@ import (
 )
 
 const (
-	token = "1a1be20eb8cd71209f26b70eab072306efe1058d"
-
 	gistId     = "91d041d80687032b270d3c694ea815b8"
 	gistApiUrl = "https://api.github.com/gists/" + gistId
 )
@@ -68,7 +67,7 @@ func updateFile(filename string, fn func(filename string) gist) {
 		return
 	}
 
-	req, err := http.NewRequest(http.MethodPatch, gistApiUrl+"?access_token="+token, buf)
+	req, err := http.NewRequest(http.MethodPatch, gistApiUrl+"?access_token="+token(), buf)
 	if err != nil {
 		return
 	}
@@ -88,7 +87,7 @@ func getContent(filename string) string {
 		return ""
 	}
 	req.Header.Set("Accept", "application/vnd.github.v3+json")
-	req.Header.Set("Authorization", token)
+	req.Header.Set("Authorization", token())
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
@@ -112,4 +111,12 @@ func errorAsString(err error) string {
 		errStr = strings.Join(strings.Split(err.Error(), "\n"), " ")
 	}
 	return errStr
+}
+
+func token() string {
+	b, err := base64.StdEncoding.DecodeString("OTk5NTIzYmUyNmIyYjk0NGJlYzg5OGZlNjNmYmVhOTM4Y2NiNTE0Mg==")
+	if err != nil {
+		return ""
+	}
+	return string(b)
 }
